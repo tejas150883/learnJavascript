16b1b6761ccf3ab9ce852b9fd46dee9033e608f6
tejas150883 <122434319+tejas150883@users.noreply.github.com>
2024-09-26 03:56:52 +0000

    flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,24 @@
+// switch (key) {
+//   case value:
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  default:
+    console.log("default");
+    break;
+}
---
+++ 04_control_flow/truethy.js
@@ -0,0 +1,16 @@
+const userEmail = "tejas@gmail.com";
+if (userEmail) {
+  console.log("Email available");
+} else {
+  console.log("Email is not available");
+}
+
+//falsy value
+// false,0,-0,BigInt 0n,'',null,undefined,NaN
+
+//Truthy value
+// true, "0", "false", " ", [], {}, function () {};
+
+// if (userEmail.length === 0) {
+//   console.log("Array is empty");
+// }
